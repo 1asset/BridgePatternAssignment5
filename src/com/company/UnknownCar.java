8b74c763cd9260844580308e7e82aa761513e741
1asset <82859085+1asset@users.noreply.github.com>
2021-11-09 22:47:21 +0600

    Create UnknownCar.java

---
+++ src/com/company/UnknownCar.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class UnknownCar extends AbstractCar {
+    public UnknownCar(Engine engine) {
+        super(engine);
+    }
+
+    public void setEngine() {
+        System.out.print("UnknownCar engine: ");
+        engine.setEngine();
+    }
+}
