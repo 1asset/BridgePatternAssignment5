d98b37b683b05a3773d5d44df7a5904f4dfd638d
1asset <82859085+1asset@users.noreply.github.com>
2021-11-09 22:45:41 +0600

    Create SportCar.java

---
+++ src/com/company/SportCar.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class SportCar extends AbstractCar {
+    public SportCar(Engine engine) {
+        super(engine);
+    }
+
+    public void setEngine() {
+        System.out.print("SportCar engine: ");
+        engine.setEngine();
+    }
+}
